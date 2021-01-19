b7c3e6befd07122fe548b252d14d068b3940121b
sion <sion007005@gmail.com>
2021-01-19 17:40:23 +0900

    작가 이름으로 도서를 검색하는 기능 추가(BookService)

--- domain/src/test/java/sion/bookstore/domain/book/service/BookServiceTest.java
+++ domain/src/test/java/sion/bookstore/domain/book/service/BookServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import sion.bookstore.domain.ApplicationConfiguration;
+import sion.bookstore.domain.book.repository.Author;
 import sion.bookstore.domain.book.repository.Book;
 import sion.bookstore.domain.book.repository.BookCategory;
 import sion.bookstore.domain.book.repository.BookRepository;
@@ -28,6 +29,10 @@ public class BookServiceTest {
     private BookCategoryService bookCategoryService;
     @Autowired
     private BookRepository bookRepository;
+    @Autowired
+    private AuthorService authorService;
+    @Autowired
+    private BookService bookService;
 
     @Test
     public void test() {
@@ -47,6 +52,25 @@ public class BookServiceTest {
         }
 
         assertEquals(20, bookList.size());
+    }
+
+    @Test
+    public void findAllByAuthorTest() {
+        AuthorSearchCondition condition = new AuthorSearchCondition();
+        condition.setName("정홍");
+
+        List<Author> authors = authorService.findAllByName(condition);
+        List<Book> bookList = new ArrayList<>();
+
+        for (Author author : authors) {
+            Book book = bookService.findOne(author.getBookId());
+            bookList.add(book);
+        }
+
+        for (Book book : bookList) {
+            log.info("책 이름: {}", book.getTitle());
+        }
+
     }
 
 }
