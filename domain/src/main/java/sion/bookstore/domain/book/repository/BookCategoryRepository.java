5715f8af0321b8d0848bb1ddba3c02961ae27b23
sion <sion007005@gmail.com>
2021-01-21 11:04:13 +0900

    ThemaSectionService 리팩토링, TODO LIST 추가

--- domain/src/main/java/sion/bookstore/domain/book/repository/BookCategoryRepository.java
+++ domain/src/main/java/sion/bookstore/domain/book/repository/BookCategoryRepository.java
@@ -2,13 +2,10 @@ package sion.bookstore.domain.book.repository;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
-import sion.bookstore.domain.book.service.BookCategorySearchCondition;
-
-import java.util.List;
 
 @Mapper
 @Repository
 public interface BookCategoryRepository {
     Long create(BookCategory mapping);
-    List<BookCategory> findBooksByCategoryId(BookCategorySearchCondition condition);
+//    List<BookCategory> findBooksByCategoryId(BookCategorySearchCondition condition);
 }
