86fa31c7b9b02b0fb4620490c32c7e799d0c4eee
sion <sion007005@gmail.com>
2021-02-16 15:24:08 +0900

    장바구니에 상품 추가 시 memberId를 클라이언트에서 받아오지않고, 서버에서 현재 로그인한 멤버 정보를 가지고 세팅한다

--- domain/src/main/java/sion/bookstore/domain/cart/service/CartService.java
+++ domain/src/main/java/sion/bookstore/domain/cart/service/CartService.java
@@ -7,7 +7,6 @@ import sion.bookstore.domain.BaseAuditor;
 import sion.bookstore.domain.auth.UserContext;
 import sion.bookstore.domain.cart.repository.CartItem;
 import sion.bookstore.domain.cart.repository.CartRepository;
-import sion.bookstore.domain.utils.validator.ValidationException;
 
 import java.util.List;
 import java.util.Objects;
@@ -20,15 +19,12 @@ public class CartService {
 
     public Long add(CartItem cart) {
         //TODO validation check
-        if (cart.getMemberId() != UserContext.get().getMemberId()) {
-            cart.setMemberId(UserContext.get().getMemberId());
-            throw new ValidationException("잘못된 요청입니다.");
-        }
+        cart.setMemberId(UserContext.get().getMemberId());
 
-        CartItem existingItem = findOneByCartId(cart.getId());
+        CartItem existingItem = findOneByBookId(cart.getBookId());
         if (Objects.nonNull(existingItem)) {
-            existingItem.setDeleted(true);
-            update(existingItem);
+            existingItem.setQuantity(cart.getQuantity());
+            return update(existingItem);
         }
 
         BaseAuditor.setCreationInfo(cart);
@@ -64,6 +60,20 @@ public class CartService {
         update(cartItem);
     }
 
+    private Long update(CartItem cart) {
+        cart.setMemberId(UserContext.get().getMemberId());
+        BaseAuditor.setUpdatingInfo(cart);
+
+        cartRepository.update(cart);
+        return cart.getId();
+    }
+
+    public void removeByItemIds(List<Long> cartItemIds) {
+        for (Long cartItemId : cartItemIds) {
+            deleteCartItem(cartItemId);
+        }
+    }
+
     public void deleteCartItem(Long cartItemId) {
         CartItem cartItem = findOneByCartId(cartItemId);
         if (Objects.isNull(cartItem)) {
@@ -72,22 +82,5 @@ public class CartService {
 
         BaseAuditor.setDeletionInfo(cartItem);
         cartRepository.update(cartItem);
-    }
-
-    private void update(CartItem cart) {
-        if (cart.getMemberId() != UserContext.get().getMemberId()) {
-            throw new ValidationException("잘못된 요청입니다.");
-        }
-
-        cart.setMemberId(UserContext.get().getMemberId());
-        BaseAuditor.setUpdatingInfo(cart);
-
-        cartRepository.update(cart);
-    }
-
-    public void removeByItemIds(List<Long> cartItemIds) {
-        for (Long cartItemId : cartItemIds) {
-            deleteCartItem(cartItemId);
-        }
     }
 }
