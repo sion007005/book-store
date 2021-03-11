e657b894aa46f7ba2ac3278e94e4678fccbf85cf
sion <sion007005@gmail.com>
2021-03-11 22:04:35 +0900

    프론트 모듈과 어드민 모듈에서 모두 사용하는 메소드를 util 클래스로 분리함

--- front/src/main/java/sion/bookstore/front/controller/member/LoginController.java
+++ front/src/main/java/sion/bookstore/front/controller/member/LoginController.java
@@ -13,7 +13,6 @@ import sion.bookstore.front.ResponseData;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,24 +34,10 @@ public class LoginController {
     private Cookie getCookie(Member member) {
         String encryptedCookieValue = generalLoginService.getEncryptedCookieValue(member);
 
-        Cookie cookie = new Cookie(CookieUtils.makeCookieName(findActiveProfile(), FrontConstants.COOKIE_KEY), encryptedCookieValue);
+        Cookie cookie = new Cookie(CookieUtils.makeCookieName(CookieUtils.findActiveProfile(), FrontConstants.COOKIE_KEY), encryptedCookieValue);
         cookie.setPath("/");
         cookie.setMaxAge(-1);
 
         return cookie;
-    }
-
-    private String findActiveProfile() {
-        if(Arrays.stream(environment.getActiveProfiles()).anyMatch(
-                env -> (env.equalsIgnoreCase("local")))) {
-            return "local";
-        }
-
-        if(Arrays.stream(environment.getActiveProfiles()).anyMatch(
-                env -> (env.equalsIgnoreCase("dev")) )) {
-            return "dev";
-        }
-
-        return "";
     }
 }
